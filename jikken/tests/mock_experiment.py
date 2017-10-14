8561d67e71aa3bffe20be4f3f76077e5c4259045
Agis <agis.pof@gmail.com>
2017-10-14 10:46:03 +0100

    added json test and raises erro test

--- jikken/tests/mock_experiment.py
+++ jikken/tests/mock_experiment.py
@@ -1,5 +1,14 @@
 from jikken import cli
 
 @cli.experiment(experiment_filepath="./tests/experiment.yaml")
-def main(**kwargs):
+def main_yaml(**kwargs):
+    return kwargs
+
+
+@cli.experiment(experiment_filepath="./tests/experiment.json")
+def main_json(**kwargs):
+    return kwargs
+
+@cli.experiment(experiment_filepath="./tests/experiment")
+def main_error(**kwargs):
     return kwargs
--- jikken/tests/test_cli.py
+++ jikken/tests/test_cli.py
@@ -1,20 +1,31 @@
-from .mock_experiment import main
-import yaml
+import pytest
+
+from .mock_experiment import main_yaml, main_json, main_error
+
+
 class TestCli:
-    def test_default_cli(self):
-        results = main()
+    @pytest.mark.parametrize("test_func", [
+        main_yaml,
+        main_json,
+    ])
+    def test_default_cli(self, test_func):
+        results = test_func()
         expected_dict = {
             "training_parameters":
-                {"batch_size":100,
+                {"batch_size": 100,
                  "algorithm": "Seq2Seq",
                  "attention": "multiplicative"
                  },
             "input_parameters":
-                {'batch_size':4,
+                {'batch_size': 4,
                  "filepath": "/data",
-                 "preprocessing":True,
+                 "preprocessing": True,
                  "transformations": ["stopwords", "tokenize", "remove_punct"]
                  }
 
         }
         assert results == expected_dict
+
+    def test_error_cli(self):
+        with pytest.raises(IOError):
+            results = main_error()
