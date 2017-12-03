f85ea93afb060b22ec064421f6736f35dc145930
Agis <agis.pof@gmail.com>
2017-12-03 11:49:03 +0000

    changed version in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='jikken',
-      version='0.1.0',
+      version='0.2.0',
       license='BSD',
       description='Minimal Python3 CLI Experiment Manager',
       author='Agis Oikonomou',
